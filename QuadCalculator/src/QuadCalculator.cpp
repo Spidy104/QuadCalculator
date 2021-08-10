d65fc422f294eb7a8a5b2df546fcfeb4fd51c226
Dibyojyoti Bhattacharjee <scientistdibs@outlook.com>
2021-08-10 07:29:24 +0530

    Some changes

--- QuadCalculator/src/QuadCalculator.cpp
+++ QuadCalculator/src/QuadCalculator.cpp
@@ -30,7 +30,7 @@ double discriminant(double a, double b, double c) {
 void printHelpMessage() {
 	vector<string> helplines = {
 		"Input values are the Quadratic coefficients, a,b,c",
-		"For real values of a,b,c which have complex roots/real roots enter like: .'\'QuadCalculator.exe -r 1 2 3 ",
+		"For real values of a,b,c ",
 		"For complex values of a,b,c just write .'\'QuadCalculator.exe -cc 1,2 3,4 5,6 here, u have to write the argand coordinates of a,b and c",
 		"One tip, if u wanna use square roots in the coefficients, use like 2sqrt3 if it's just sqrt of a number just use then 1sqrtnum"
 	};
